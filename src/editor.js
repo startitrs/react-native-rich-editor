c819a5263214fcd0edea834797b3df3435500835
Filip Ilic <ilic.filip@gmail.com>
2023-05-19 09:51:38 +0200

    prevent adding <span style="font-size: 1em;"></span> after mention is used

--- src/editor.js
+++ src/editor.js
@@ -55,6 +55,7 @@ function createHTML(options = {}) {
         * {outline: 0px solid transparent;-webkit-tap-highlight-color: rgba(0,0,0,0);-webkit-touch-callout: none;box-sizing: border-box;}
         html, body { margin: 0; padding: 0;font-family: Arial, Helvetica, sans-serif; font-size:1em; height: 100%}
         body { overflow-y: hidden; -webkit-overflow-scrolling: touch;background-color: ${backgroundColor};caret-color: ${caretColor};}
+        span {font-size: 1em;}
         .content {font-family: Arial, Helvetica, sans-serif;color: ${color}; width: 100%;${
     !useContainer ? 'height:100%;' : ''
   }-webkit-overflow-scrolling: touch;padding-left: 0;padding-right: 0;}
